e821f7af27cd86b512f24d693ffcf365a6639f14
fratiro <feras.el.mokdad@gmail.com>
2023-06-30 17:26:02 +0300

    finish UI

--- lib/Features/Search/Presentation/Views/Widgets/search_body.dart
+++ lib/Features/Search/Presentation/Views/Widgets/search_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/Core/Utilities/styles.dart';
 import 'package:book_app/Features/Search/Presentation/Views/Widgets/search_list_view.dart';
 import 'package:flutter/material.dart';
 import 'custom_text_field.dart';
@@ -11,8 +12,14 @@ class SearchViewBody extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CustomTextField(),
+            SizedBox(height: 16),
+            Text(
+              "Search Result",
+              style: Styles.textStyle18,
+            ),
             SizedBox(height: 16),
             SearchListView(),
           ],
