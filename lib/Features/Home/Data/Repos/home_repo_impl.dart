3954eba17e145bb2fa49628dd6489a25157cd664
fratiro <feras.el.mokdad@gmail.com>
2023-07-01 23:03:30 +0300

    implemet fetch newest books

--- lib/Features/Home/Data/Repos/home_repo_impl.dart
+++ lib/Features/Home/Data/Repos/home_repo_impl.dart
@@ -3,11 +3,28 @@ import 'package:book_app/Features/Home/Data/Model/book_model/book_model.dart';
 import 'package:book_app/Features/Home/Data/Repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../../../Core/Utilities/api_service.dart';
+
 class HomeRepoImple implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImple(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: "volumes?Filtering=free-ebooks&subject=&q=Programming");
+
+      List<BookModel> books = [];
+
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
